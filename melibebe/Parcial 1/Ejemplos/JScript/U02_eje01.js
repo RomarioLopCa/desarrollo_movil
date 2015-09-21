153d340723ce650c8d86346212379f107e8d44d4
RomarioLopCa <romario_lopezc@hotmail.com>
2015-09-21 15:10:51 -0500

    Identar todo el proyecto.

--- melibebe/Parcial 1/Ejemplos/JScript/U02_eje01.js
+++ melibebe/Parcial 1/Ejemplos/JScript/U02_eje01.js
@@ -1,6 +1,6 @@
 function makeCar() {
-    var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"]; 
-    var models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"]; 
+    var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+    var models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
     var years = [1955, 1957, 1948, 1954, 1961];
     var colors = ["red", "blue", "tan", "yellow", "white"];
     var convertible = [true, false];
@@ -14,17 +14,17 @@ function makeCar() {
         make: makes[rand1],
         model: models[rand2],
         year: years[rand3],
-        color: colors[rand4], 
+        color: colors[rand4],
         passengers: rand5,
-        convertible: convertible[rand6], 
+        convertible: convertible[rand6],
         mileage: 0
     };
-    return car; 
+    return car;
 }
 
 function displayCar(car) {
-    console.log("Your new car is a " + car.year + " " + car.make + " " + car.model); 
+    console.log("Your new car is a " + car.year + " " + car.make + " " + car.model);
 }
 
-var carToSell = makeCar(); 
+var carToSell = makeCar();
 displayCar(carToSell);
--- melibebe/Parcial 1/Ejercicios/Ejercicios_JQuery/js/eje01.js
+++ melibebe/Parcial 1/Ejercicios/Ejercicios_JQuery/js/eje01.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $("#moveUp").click(function(){
-        $("#changeMe").animate({top:30},2000);
+$(document).ready(function () {
+    $("#moveUp").click(function () {
+        $("#changeMe").animate({top: 30}, 2000);
     });
-    $("#moveDown").click(function(){
-        $("#changeMe").animate({top:500},2000);
+    $("#moveDown").click(function () {
+        $("#changeMe").animate({top: 500}, 2000);
     });
-    $("#color").click(function(){
-        $("#changeMe").css("color","blue");
+    $("#color").click(function () {
+        $("#changeMe").css("color", "blue");
     });
-    $("#disappear").click(function(){
+    $("#disappear").click(function () {
         $("#changeMe").toggle("slow");
     });
 });
